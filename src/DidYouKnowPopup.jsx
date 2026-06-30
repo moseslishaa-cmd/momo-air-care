74552c23a0ba1a9cdd41cf4538efc45c6d475779
Moses Lishaa <moses.lishaa@gmail.com>
2026-06-29 23:11:14 -0700

    Use fresh filename for popup mascot to bust cache

--- src/DidYouKnowPopup.jsx
+++ src/DidYouKnowPopup.jsx
@@ -107,7 +107,7 @@ export function DidYouKnowPopup({ triggerRef }) {
                 position: 'relative', overflow: 'hidden',
               }}>
                 <img
-                  src="/momo-faq-gold.png"
+                  src="/momo-popup-mascot.png"
                   alt="MOMO mascot"
                   style={{
                     position: 'absolute',
